bda396672215afd4cb05f2063ae65e4492d3cf74
zakdok <zakdok@tinto.co.kr>
2020-07-13 19:13:48 +0900

    27강 끝

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 5000;
 const bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser");
 const config = require('./config/key');
@@ -101,5 +100,7 @@ app.get('/api/users/logout', auth, (req, res) => {
     );
 
 });
+
+const port = 5000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
